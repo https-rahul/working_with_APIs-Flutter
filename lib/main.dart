acb5df2b71dcfa171d1961a1aa23263bed319c7a
Rahul Gaur <rgaur1620@gmail.com>
2020-05-31 15:15:08 +0530

    made necessary changes

--- lib/main.dart
+++ lib/main.dart
@@ -24,61 +24,55 @@ void main() async {
         centerTitle: true,
         backgroundColor: Colors.orangeAccent,
       ),
-      body: new Column(children: <Widget>[
-        new Container(
-            child:
-                new Text("apiURL: https://jsonplaceholder.typicode.com/posts")),
-        new Container(
-            child: new ListView.builder(
-                itemCount: _data.length,
-                padding: const EdgeInsets.all(14.5),
-                itemBuilder: (BuildContext context, int position) {
-                  return Column(
-                    children: <Widget>[
-                      new Divider(height: 5.5),
-                      new ListTile(
-                        title: Text(
-                          "${_data[position]['title']}",
-                          style: new TextStyle(fontSize: 17.9),
+      body: new Center(
+          child: new ListView.builder(
+              itemCount: _data.length,
+              padding: const EdgeInsets.all(14.5),
+              itemBuilder: (BuildContext context, int position) {
+                return Column(
+                  children: <Widget>[
+                    new Divider(height: 5.5),
+                    new ListTile(
+                      title: Text(
+                        "${_data[position]['title']}",
+                        style: new TextStyle(fontSize: 17.9),
+                      ),
+                      subtitle: Text("${_data[position]['body']}",
+                          style: new TextStyle(
+                              fontSize: 13.9,
+                              color: Colors.grey,
+                              fontStyle: FontStyle.italic)),
+                      leading: new CircleAvatar(
+                        backgroundColor: Colors.greenAccent,
+                        child: Text(
+                          "${_data[position]['id']}",
+                          style: new TextStyle(
+                              fontSize: 16.4, color: Colors.orangeAccent),
                         ),
-                        subtitle: Text("${_data[position]['body']}",
-                            style: new TextStyle(
-                                fontSize: 13.9,
-                                color: Colors.grey,
-                                fontStyle: FontStyle.italic)),
-                        leading: new CircleAvatar(
-                          backgroundColor: Colors.deepOrangeAccent,
-                          child: Text(
-                            "${_data[position]['id']}",
-                            style: new TextStyle(
-                                fontSize: 16.4, color: Colors.white),
-                          ),
-                        ),
-                        onTap: () =>
-                            _showonTapMessage(context, _data[position]['body']),
-                      )
-                    ],
-                  );
-                })),
-      ]),
+                      ),
+                      onTap:() => _showonTapMessage(context, _data[position]['body']),
+
+                    )
+                  ],
+                );
+              })),
     ),
   ));
 }
 
 void _showonTapMessage(BuildContext context, String message) {
   var alert = new AlertDialog(
-    title: Text("My App"),
-    content: Text(message),
-    actions: <Widget>[
-      FlatButton(
-        child: Text("OK"),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )
-    ],
+     title: Text("My App"),
+     content: Text(message),
+      actions: <Widget>[
+         FlatButton(
+           child: Text("OK"),
+           onPressed: () {
+              Navigator.pop(context);
+           }, )
+      ],
   );
-  // showDialog(context: context, child: alert);
+ // showDialog(context: context, child: alert);
   showDialog(context: context, builder: (context) => alert);
 }
 
@@ -88,4 +82,39 @@ Future<List> getJson() async {
   http.Response response = await http.get(apiUrl);
 
   return json.decode(response.body); // returns a List type
+}
+
+//showDialog(context: context, builder: (context) {
+//return alert;
+////     return new AlertDialog(
+////      title: new Text('App'),
+////      content: new Text(message),
+////      actions: <Widget>[
+////        new FlatButton(
+////            onPressed: () {
+////              Navigator.pop(context);
+////            },
+////            child: new Text('OK'))
+////      ],
+////    );
+//});
+
+/*
+   void _showOnTapMessage(BuildContext context, String message) {
+  var alert = new AlertDialog(
+    title: new Text('App'),
+    content: new Text(message),
+    actions: <Widget>[
+      new FlatButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: new Text('OK'))
+    ],
+  );
+  //showDialog(context: context, child: alert);
+  showDialog(context: context, builder: (context) => alert);
+
+
+
 }
